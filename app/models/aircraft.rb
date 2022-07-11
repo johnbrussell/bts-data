cfecaddc40d279b5a26b9268fb8ff8b9e031f514
JBR <john.russell@tufts.edu>
2022-07-11 18:27:46 -0400

    Select_options for each model

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -4,4 +4,10 @@ class Aircraft < ApplicationRecord
   validates :name, presence: true
   validates :group, presence: true
   validates :bts_id, presence: true
+
+  def self.select_options
+    all.order(:name).map do |aircraft|
+      [aircraft.name, aircraft.id]
+    end
+  end
 end
