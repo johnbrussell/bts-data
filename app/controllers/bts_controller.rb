5dbcfff7a98c48205e497afac6fa3920b8aad266
JBR <john.russell@tufts.edu>
2022-07-13 21:12:45 -0400

    Filter for flights operated at least weekly

--- app/controllers/bts_controller.rb
+++ app/controllers/bts_controller.rb
@@ -14,6 +14,7 @@ class BtsController < ApplicationController
     @departure_airport = if params[:departure_airport].present? then Airport.find(params[:departure_airport]) else nil end
     @arrival_airport = if params[:arrival_airport].present? then Airport.find(params[:arrival_airport]) else nil end
     @aircraft_type = if params[:aircraft_type].present? then Aircraft.find(params[:aircraft_type]) else nil end
+    @filter_for_weekly = params[:filter_for_weekly] == "1"
 
     @airlines = Airline.select_options
     @airports = Airport.select_options
@@ -26,6 +27,6 @@ class BtsController < ApplicationController
     if params[:group_by_airline] == "1" then @groups << "airlines.iata" end
     if params[:group_by_aircraft] == "1" then @groups << "aircraft.name" end
 
-    @results = Counts.meeting_specific_criteria(@start_month, @start_year, @end_month, @end_year, @airline&.id, @aircraft_type&.id, @departure_airport&.id, @arrival_airport&.id, @groups)
+    @results = Counts.meeting_specific_criteria(@start_month, @start_year, @end_month, @end_year, @airline&.id, @aircraft_type&.id, @departure_airport&.id, @arrival_airport&.id, @groups, @filter_for_weekly)
   end
 end
