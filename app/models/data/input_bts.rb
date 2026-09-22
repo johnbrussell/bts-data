9e9ecc89d6b69c1fc06c3f7152031907b948df9c
John Russell <14211416+johnbrussell@users.noreply.github.com>
2026-09-21 20:12:41 -0400

    allow for multiple files to be processed (#22)

--- app/models/data/input_bts.rb
+++ app/models/data/input_bts.rb
@@ -25,58 +25,19 @@ class Data::InputBts < ApplicationRecord
   }
 
   def self.run
-    aircraft = Aircraft.all.filter{|a| a.name.exclude?(COMBI_SUFFIX)}.map{ |a| {a.bts_id => a.attributes.slice("bts_id", "name", "group", "id")}}.reduce({}, :merge)
-    airlines = Airline.all.pluck(:iata, :id).to_h
-    airports = Airport.all.pluck(:iata, :id).to_h
-    time_periods = TimePeriod.all.pluck(:name, :id).to_h
-
-    self.files.each do |f|
-      records = []
-
-      CSV.foreach(f, headers: false, col_sep: "|") do |d|
-        if d[DATA_LOCATION[:departures_scheduled]].to_i > 0 && VALID_SERVICE_TYPES.include?(d[DATA_LOCATION[:service_type]])
-          aircraft_id = self.get_aircraft_id(aircraft, d[DATA_LOCATION[:aircraft_group]], d[DATA_LOCATION[:aircraft_bts_id]], d[DATA_LOCATION[:aircraft_configuration_type]])
-          airline_id = self.get_airline_id(airlines, d[DATA_LOCATION[:airline_iata]])
-          origin_airport_id = self.get_airport_id(airports, d[DATA_LOCATION[:origin_iata]], d[DATA_LOCATION[:origin_name]])
-          destination_airport_id = self.get_airport_id(airports, d[DATA_LOCATION[:destination_iata]], d[DATA_LOCATION[:destination_name]])
-          time_period_id = self.get_time_period(time_periods, d[DATA_LOCATION[:month]], d[DATA_LOCATION[:year]])
-
-          time = Time.now
-          records << {
-            aircraft_id: aircraft_id,
-            airline_id: airline_id,
-            origin_airport_id: origin_airport_id,
-            destination_airport_id: destination_airport_id,
-            time_period_id: time_period_id,
-            departures_performed: d[DATA_LOCATION[:departures_performed]],
-            departures_scheduled: d[DATA_LOCATION[:departures_scheduled]],
-            seats: d[DATA_LOCATION[:seats]],
-            passengers: d[DATA_LOCATION[:passengers]],
-            created_at: time,
-            updated_at: time,
-          }
-        end
-
-        if records.length >= BATCH_SIZE
-          tuples_to_check = records.map{ |r| [r[:aircraft_id], r[:airline_id], r[:origin_airport_id], r[:destination_airport_id], r[:time_period_id]]}
-          bind_placeholders = Array.new(tuples_to_check.size, "(?, ?, ?, ?, ?)").join(", ")
-          existing_records = Counts.where("(aircraft_id, airline_id, origin_airport_id, destination_airport_id, time_period_id) in (#{bind_placeholders})", *(tuples_to_check.flatten)).pluck(:aircraft_id, :airline_id, :origin_airport_id, :destination_airport_id, :time_period_id)
-          records = records.filter{ |r| existing_records.exclude?([r[:aircraft_id], r[:airline_id], r[:origin_airport_id], r[:destination_airport_id], r[:time_period_id]]) }
-
-          Counts.insert_all! records if records.length > 0
-          records = []
-        end
-      end
-
-      Counts.insert_all! records if records.length > 0
-      ProcessedFile.create!(name: f)
+    self.unfinished_files.each do |f|
+      self.insert_file_data(f)
     end
   end
 
-  def self.files
-    processed_files = ProcessedFile.pluck(:name)
-
-    Dir.glob("data/bts/*").reject { |f| processed_files.include?(f) }
+  def self.run_once
+    processed_file_count = ProcessedFile.count
+    self.unfinished_files.each do |f|
+      self.insert_file_data(f)
+      if ProcessedFile.count > processed_file_count
+        break
+      end
+    end
   end
 
   def self.get_aircraft_id(aircraft, group, bts_id, config_type)
@@ -132,5 +93,58 @@ class Data::InputBts < ApplicationRecord
 
   def self.get_time_period_name(month, year)
     ((year.to_s + (month.to_i + 10).to_s).to_i - 10).to_s
+  end
+
+  def self.insert_file_data(file)
+    aircraft = Aircraft.all.filter{|a| a.name.exclude?(COMBI_SUFFIX)}.map{ |a| {a.bts_id => a.attributes.slice("bts_id", "name", "group", "id")}}.reduce({}, :merge)
+    airlines = Airline.all.pluck(:iata, :id).to_h
+    airports = Airport.all.pluck(:iata, :id).to_h
+    time_periods = TimePeriod.all.pluck(:name, :id).to_h
+
+    records = []
+
+    CSV.foreach(file, headers: false, col_sep: "|") do |d|
+      if d[DATA_LOCATION[:departures_scheduled]].to_i > 0 && VALID_SERVICE_TYPES.include?(d[DATA_LOCATION[:service_type]])
+        aircraft_id = self.get_aircraft_id(aircraft, d[DATA_LOCATION[:aircraft_group]], d[DATA_LOCATION[:aircraft_bts_id]], d[DATA_LOCATION[:aircraft_configuration_type]])
+        airline_id = self.get_airline_id(airlines, d[DATA_LOCATION[:airline_iata]])
+        origin_airport_id = self.get_airport_id(airports, d[DATA_LOCATION[:origin_iata]], d[DATA_LOCATION[:origin_name]])
+        destination_airport_id = self.get_airport_id(airports, d[DATA_LOCATION[:destination_iata]], d[DATA_LOCATION[:destination_name]])
+        time_period_id = self.get_time_period(time_periods, d[DATA_LOCATION[:month]], d[DATA_LOCATION[:year]])
+
+        time = Time.now
+        records << {
+          aircraft_id: aircraft_id,
+          airline_id: airline_id,
+          origin_airport_id: origin_airport_id,
+          destination_airport_id: destination_airport_id,
+          time_period_id: time_period_id,
+          departures_performed: d[DATA_LOCATION[:departures_performed]],
+          departures_scheduled: d[DATA_LOCATION[:departures_scheduled]],
+          seats: d[DATA_LOCATION[:seats]],
+          passengers: d[DATA_LOCATION[:passengers]],
+          created_at: time,
+          updated_at: time,
+        }
+      end
+
+      if records.length >= BATCH_SIZE
+        tuples_to_check = records.map{ |r| [r[:aircraft_id], r[:airline_id], r[:origin_airport_id], r[:destination_airport_id], r[:time_period_id]]}
+        bind_placeholders = Array.new(tuples_to_check.size, "(?, ?, ?, ?, ?)").join(", ")
+        existing_records = Counts.where("(aircraft_id, airline_id, origin_airport_id, destination_airport_id, time_period_id) in (#{bind_placeholders})", *(tuples_to_check.flatten)).pluck(:aircraft_id, :airline_id, :origin_airport_id, :destination_airport_id, :time_period_id)
+        records = records.filter{ |r| existing_records.exclude?([r[:aircraft_id], r[:airline_id], r[:origin_airport_id], r[:destination_airport_id], r[:time_period_id]]) }
+
+        Counts.insert_all! records if records.length > 0
+        records = []
+      end
+    end
+
+    Counts.insert_all! records if records.length > 0
+    ProcessedFile.create!(name: file)
+  end
+
+  def self.unfinished_files
+    processed_files = ProcessedFile.pluck(:name)
+
+    Dir.glob("data/bts/*").reject { |f| processed_files.include?(f) }
   end
 end
